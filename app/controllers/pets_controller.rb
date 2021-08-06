18151fccb5ec0f9b1fc547c862dcd276679a55f6
Karina Gonzalez <KarxGonzo@gmail.com>
2021-08-05 20:56:46 -0400

    Adding Show/Index/Updates Controller/Routes for Pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,16 +1,35 @@
 class PetsController < ApplicationController
-  before_action :authenticate_user, only: [:create]
+  before_action :authenticate_user, only: [:create, :show, :index, :update]
+
+  def index
+    pet = Pet.all
+    render json: pet.as_json
+  end
 
   def create
-    pet = Pet.new(
-      user_id: current_user.id,
-      name: params[:name],
-      picture: params[:picture]
+    if current_user
+      pet = Pet.new(
+        user_id: current_user.id,
+        name: params[:name],
+        picture: params[:picture]
     )
-    if pet.save
+      pet.save
       render json: { message: "Pet created successfully." }, status: :created
     else
       render json: { message: "Pet was not created." }, status: :bad_request
     end
+  end
+
+  def show
+    pet = Pet.find_by(id: params[:id])
+    render json: pet.as_json
+  end
+
+  def update
+    pet = Pet.find_by(id: params[:id])
+    pet.name = params[:name]
+    pet.picture = params[:picture]
+    pet.save
+    render json: pet.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   post "/sessions" => "sessions#create"
 
   post "/pets" => "pets#create"
+  get "/pets" => "pets#index"
+  get "/pets/:id" => "pets#show"
+  patch "/pets/:id" => "pets#update"
+
+
   post "/shopping_histories" => "shopping_histories#create"
   post "/items" => "items#create"
   post "/favorite_snacks" => "favorite_snacks#create"
